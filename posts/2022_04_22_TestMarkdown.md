f7ad9694cad0d465ff5bcfa6a594c4e99f2aa7c9
Acorn <github@unallied.com>
2023-02-01 08:44:02 -0800

    Add initial posts to repo

---
+++ posts/2022_04_22_TestMarkdown.md
@@ -0,0 +1,38 @@
+# Hello World
+Because it's a demo. Just testing out markdown on Foony.
+
+## Second Heading
+This heading is smaller than the main one.
+
+### Third Heading
+Smaller than second.
+
+[This is a link.](https://foony.com)
+
+**bold text**
+
+*italics*, _also italics_
+
+>this is a quote in a block.
+>>nested block quote.
+>>>nested again.
+
+1. Numbered list of things.
+2. Many numbers.
+
+* Bullet list.
+* Bullets.
+
+- Bullet list again.
+- More bullets.
+
+`a piece of code`
+
+```typescript
+// This is TypeScript.
+const foo = 'some typescript.';
+```
+
+---
+
+![DinoMight thumbnail](https://foony.com/img/games/dinomight/thumbnail.webp)
---
+++ posts/index.ts
@@ -0,0 +1,32 @@
+import {kebabCase} from 'lodash';
+
+const allPosts = require.context('!@mdx-js/loader!posts', true, /(\.mdx|\.md)$/, 'lazy');
+
+type PostType = {
+  /** The Date this post was created at. Posts follow the format "yyyy_mm_dd_PostTitle.md". */
+  createdAt: Date,
+  /** Returns a promise that will resolve to the component when resolved. */
+  lazyComponent: () => Promise<() => JSX.Element>,
+};
+
+// TODO(acorn1010): Consider caching the responses for this.
+export const POSTS: {[articleId: string]: PostType} =
+    Object.fromEntries(
+        allPosts.keys()
+            .filter(path => path.startsWith('./'))
+            .map(path => {
+              const lastUnderscore = path.lastIndexOf('_');
+              const [yyyy, mm, dd] = path.slice('./'.length, lastUnderscore).split('_');
+              // The date this post was made. If undefined, arbitrarily starts at the Unix timestamp.
+              if (yyyy.length !== 4 || !mm || !dd || +mm > 12 || +mm < 1 || +dd < 1 || +dd > 31) {
+                console.error(`Post has unknown date format: "${path}"`);
+              }
+              const createdAt = new Date(Date.UTC(+(yyyy || 1970), +(mm || 1) - 1, +(dd || 1)));
+              const slug = kebabCase(path.slice(lastUnderscore + 1, path.lastIndexOf('.') - path.length));
+
+              // noinspection JSUnusedGlobalSymbols This is used in PostPage.tsx.
+              return [
+                slug,
+                {createdAt, lazyComponent: () => allPosts(path).then((module: any) => module.default)},
+              ];
+            }));
